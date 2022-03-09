73c4818eaf35e8b125734b7883e7a8f8e6e7ba13
elzup <guild0105@gmail.com>
2022-03-09 10:37:50 +0900

    add surround stage

---
+++ src/stages/surrounds/ok.js
@@ -0,0 +1,17 @@
+// happy surrounds
+fn = () => {}
+
+fn(100)
+fn()
+fn('fun')
+
+fn(200)
+const me = 0
+fn('embed me')
+fn("'a', 'b', 'c'")
+fn(['a', 'b', 'c'])
+
+fn(300)
+fn(() => ({}))
+fn(() => ({}))
+fn(() => ({ ok: true }))
