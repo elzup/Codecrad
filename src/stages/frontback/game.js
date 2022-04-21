a5abf4c7fc61ddd7105fb7e937ae1052b1ae3f2a
elzup <guild0105@gmail.com>
2022-04-21 12:15:16 +0900

    add stage

---
+++ src/stages/frontback/game.js
@@ -0,0 +1,7 @@
+const helloDeleteMeWorld = () => {}
+const helloYoDeleteWorld = () => {}
+const message = 'hello del me world'
+
+const OneTwoOrangeThree = 'tick4tock'
+const deleteAbc = 'tick a b c tock'
+const deleteNums = 'tick 1 2 tock 1 2 tic'
---
+++ src/stages/frontback/ok.js
@@ -0,0 +1,7 @@
+const helloWorld = () => {}
+const helloYoWorld = () => {}
+const message = 'hello world'
+
+const OneTwoThree = 'ticktock'
+const deleteAbc = 'tick tock'
+const deleteNums = 'tick tock tic'
